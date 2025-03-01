44872b0e1df6cdcab521e5a01fe8120f1540039e
Bilong Emmanuel <emmanueldigital9@gmail.com>
2025-03-01 12:45:22 +0100

    Initiate backend project

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './database/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { LeavesModule } from './leaves/leaves.module';
+import { TimesheetsModule } from './timesheets/timesheets.module';
+import { ReportsModule } from './reports/reports.module';
+import { SearchModule } from './search/search.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { EvaluationsModule } from './evaluations/evaluations.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+    AuthModule,
+    UsersModule,
+    LeavesModule,
+    TimesheetsModule,
+    ReportsModule,
+    SearchModule,
+    NotificationsModule,
+    EvaluationsModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModuleAsyncOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        autoLoadEntities: true,
+        synchronize: true,
+    })
+}
